b3f4ea7c848e128347ea9f1d44c064c2d069f717
JAEJUN0124 <jaejuno123w@gmail.com>
2025-05-15 12:58:13 +0900

    11주차 수업

---
+++ lib/presentation/widgets/app_scaffod.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppScaffod extends StatelessWidget {
+  final Widget child;
+
+  const AppScaffod({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: child));
+  }
+}
